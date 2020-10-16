f8f49b95723fdbe61b8f93e003fe5555cda5b6d7
Yumeng <xaiweiok@vip.qq.com>
2020-10-16 19:05:48 +0800

    更新

---
+++ src/views/DRN/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Bookmarks />
+</template>
+
+<script>
+const Bookmarks = () => import(/* webpackChunkName: "Bookmarks" */ '@/components/Bookmarks/Index');
+export default {
+  name: "Index",
+  components: {
+    Bookmarks
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
